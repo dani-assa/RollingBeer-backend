b58298fc2b4ce2f43cbfd5e1f5287b35867cede1
Facundo <santillanfacundo43@gmail.com>
2024-03-15 12:58:38 -0300

    agregando la propiedad para eliminar y disaled del usuario

--- src/validators/userValidations.js
+++ src/validators/userValidations.js
@@ -2,50 +2,16 @@ import { body } from "express-validator";
 import User from '../models/user.model.js';
 import passRegex from '../helpers/passwordRegex.js';
 
-
-const userNameValidations = body('userName').custom(async (value) => {
-  const userNameExist = await Product.findOne({ userName: value });
-  if (userNameExist) {
-    throw new Error(`El usuario ${value} ya está registrado`);
-  }
-  return true;
-}); 
-
-const dniValidations = body('dni').custom(async (value) => {
-  const dniExist = await Product.findOne({ dni: value });
-  if (dniExist) {
-    throw new Error(`El dni ${value} ya está registrado`);
-  }
-  return true;
-}); 
-
-const emailValidations = body('email').custom(async (value) => {
-  const emailExist = await Product.findOne({ email: value });
-  if (emailExist) {
-    throw new Error(`El email ${value} ya está registrado`);
-  }
-  return true;
-}); 
-
-
 const userValidations = {
-  email: body('email')
-    .notEmpty()
+  email: body("email")
+    .isEmail()
     .withMessage('El email no es valido')
-    .custom(emailValidations),
+    .not()
+    .isEmpty()
+    .withMessage('Este campo es requerido'),
   password: body('password')
-    .notEmpty()
     .matches(passRegex)
-    .withMessage('La contraseña no es valida'),
-  username: body('userName')
-    .notEmpty()
-    .withMessage('El usuario no es valido')
-    .custom(userNameValidations),
-  dni: body('dni')
-    .notEmpty()
-    .custom(dniValidations)
-    .withMessage('El dni no es valido')
+    .withMessage('La contraseña no es valida')
 };
-
 
 export default userValidations;
