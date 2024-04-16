7468b27428482be2cc4d7817402aafee5a7d1ea0
Facundo <santillanfacundo43@gmail.com>
2024-04-15 21:44:42 -0300

    corrigiendo origin de los cors

--- src/server/server.js
+++ src/server/server.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(morgan("dev"));
 app.use(
   cors({
-    origin: 'http://localhost:5173',
+    origin: 'https://main--rollingbeer1.netlify.app',
     methods: ['GET', 'POST'],
     credentials: true,
     withCredentials: true,
